5fa1d321dc5f285a645338d589125b8a2ae00548
Yapeng Lang <u7181074@gmail.com>
2024-07-18 22:28:48 +1000

    NEW: convert ete3 to C3 tree \tests for topology

--- tests/test_trs_tree.py
+++ tests/test_trs_tree.py
@@ -1,7 +1,8 @@
-from cogent3 import load_tree
+from cogent3 import load_aligned_seqs, load_tree
+from ete3 import PhyloTree
 from numpy import allclose, array, float64
 
-from myproject.trs_tree import C3ToEte3Tree
+from myproject.trs_tree import C3ToEte3Tree, Ete3ToC3Tree
 
 
 class TestsC3ToEte3Tree:
@@ -19,3 +20,37 @@ class TestsC3ToEte3Tree:
             array(ete_tree.cophenetic_matrix()[0]),
             tree.tip_to_tip_distances()[0],
         )
+
+    def test_topology(self):
+        tree = load_tree("data/test_tree.newick")
+        trs = C3ToEte3Tree()
+        ete_tree = trs(tree)
+        output = ete_tree.write(format=8)
+        expected = tree.get_newick(with_node_names=True)
+        assert output == expected
+
+    def test_branch_support(self):
+        algn = load_aligned_seqs("../tests/data/algn.fasta", moltype="protein")
+        tree = algn.quick_tree(bootstrap=10, show_progress=False)
+        trs = C3ToEte3Tree()
+        ete_tree = trs(tree)
+
+        support = {
+            node.name: node.params.get("support", 1.0)
+            for node in tree.get_edge_vector()
+        }
+
+        assert all(
+            allclose(support[node.name], node.support)
+            for node in ete_tree.traverse()
+            if node.name
+        )
+
+
+class TestsEte3ToC3Tree:
+    def test_topology(self):
+        ete_tree = PhyloTree("data/test_tree.newick", format=5)
+        trs = Ete3ToC3Tree()
+        tree_output = trs(ete_tree)
+        tree_expected = load_tree("data/test_tree.newick")
+        assert tree_expected.same_topology(tree_output)
