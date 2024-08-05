99eb678df2f2befdc5e9cf86b79e8ba4cb5f3b91
Yapeng Lang <u7181074@gmail.com>
2024-08-06 09:44:40 +1000

    ENH: add labels to the coloured edges
    ref issue#1

--- src/myproject/extension.py
+++ src/myproject/extension.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from ete3 import NodeStyle, PhyloTree
+from ete3 import NodeStyle, PhyloTree, TextFace
 
 
 class McatColour(Enum):
@@ -8,7 +8,6 @@ class McatColour(Enum):
     sympathetic = "#2065F7"
     limit = "#2065F7"
     chainsaw = "#FF0000"
-    dlc = "#000000"
 
 
 class colour_edge:
@@ -18,6 +17,7 @@ class colour_edge:
 
         for node in ete_tree.traverse():
             if node.name in mcats:
+                # add colour
                 style = NodeStyle()
                 style["hz_line_color"] = McatColour[mcats[node.name]].value
                 style["vt_line_color"] = McatColour[mcats[node.name]].value
@@ -25,3 +25,12 @@ class colour_edge:
                 style["hz_line_width"] = 2
                 node.set_style(style)
                 node.set_style(style)
+
+                # add caption
+                label = TextFace(f"{mcats[node.name]}")
+                label.opacity = 0.5  # from 0 to 1
+                label.inner_border.width = 1  # 1 pixel border
+                label.inner_border.type = 1  # dashed line
+                label.border.width = 1
+                label.background.color = "grey"
+                node.add_face(label, column=0, position="branch-top")
