8708fb0c4d8d3f2a62dc4317ce2a7bf4470566db
Yapeng Lang <u7181074@gmail.com>
2024-08-11 17:13:39 +1000

    ENH: turn  into a c3 app

--- src/myproject/extension.py
+++ src/myproject/extension.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from cogent3.app.composable import define_app
 from ete3 import NodeStyle, PhyloTree, TextFace
 
 
@@ -10,27 +11,32 @@ class McatColour(Enum):
     chainsaw = "#FF0000"
 
 
+@define_app
 class colour_edge:
     """colour certain edges based on mtx type"""
 
-    def __call__(self, ete_tree: PhyloTree, mcats: dict) -> None:
+    def __init__(self, mapping: dict[str, str]) -> None:
+        self.mapping = mapping
+
+    def main(self, ete_tree: PhyloTree) -> PhyloTree:
 
         for node in ete_tree.traverse():
-            if node.name in mcats:
+            if node.name in self.mapping:
                 # add colour
                 style = NodeStyle()
-                style["hz_line_color"] = McatColour[mcats[node.name]].value
-                style["vt_line_color"] = McatColour[mcats[node.name]].value
+                style["hz_line_color"] = McatColour[self.mapping[node.name]].value
+                style["vt_line_color"] = McatColour[self.mapping[node.name]].value
                 style["vt_line_width"] = 2
                 style["hz_line_width"] = 2
                 node.set_style(style)
                 node.set_style(style)
 
                 # add caption
-                label = TextFace(f"{mcats[node.name]}")
+                label = TextFace(f"{self.mapping[node.name]}")
                 label.opacity = 0.5  # from 0 to 1
                 label.inner_border.width = 1  # 1 pixel border
                 label.inner_border.type = 1  # dashed line
                 label.border.width = 1
                 label.background.color = "grey"
                 node.add_face(label, column=0, position="branch-top")
+        return ete_tree
