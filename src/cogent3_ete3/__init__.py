be7a34d45484989c6ac94c908c65abb29af4a846
Yapeng Lang <u7181074@gmail.com>
2024-08-15 14:24:23 +1000

    MAINT: update the __init__

--- src/cogent3_ete3/__init__.py
+++ src/cogent3_ete3/__init__.py
@@ -1,11 +1,4 @@
-"""my doctsring
+"""cogent3 plugin for ete3
 """
 
-__author__ = "Yapeng Lang"
-__copyright__ = "Copyright 2024, Yapeng Lang"
-__credits__ = ["Gavin Huttley"]
-__license__ = "BSD"
-__version__ = "2024.7"  # A DATE BASED VERSION
-__maintainer__ = "Yapeng Lang"
-__email__ = "u7181074@anu.edu.au"
-__status__ = "alpha"
+__version__ = "2024.8"
